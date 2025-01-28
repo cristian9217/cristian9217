957ef4dd95ffa1e602ff3e0c118c697890170a4c
cristian9217 <44002810+cristian9217@users.noreply.github.com>
2025-01-27 23:17:45 -0400

    Add files via upload

---
+++ courses/CECS6010/readme.md
@@ -0,0 +1,96 @@
+# Understanding Analysis Algorithms 
+## CECS 6010 - ADVANCED DESIGN AND ANALYSIS OF ALGORITHM
+-------
+
+**Objective**: To understand of advanced algorithms methods, their concepts, 
+and how they are used to engaging computational issues.
+
+The **main.py** file provides a menu to select and explore different sorting 
+algorithms. It allows users to visualize and understand how various 
+sorting methods work by processing a given string.
+
+1. __Analyze Sorting Algorithms__: This program analyzes sorting algorithms,
+which are implemented in the file **sorting.py** for string.
+
+* __Sorting a word using Insertion Sort__: chooses an element from the unsorted section
+  iteratively and places it in the sorted section at the appropriate location.
+* __Sorting a word using Merge Sort__: separates the word into smaller subwords,
+  sorts them recursively, and then reassembles the subwords that have been sorted.
+* __Sorting a word using Quick Sort__: After choosing a pivot element, divides the word
+  into smaller and larger components and sorts each partition recursively.
+
+2. __Analyze Searching Algorithms__: This program analyzes searching algortihms,
+which are implemented in the file **searching.py** for integer numbers. 
+
+* Analyze Binary Search with the average-case scenario: when there is an
+  equal chance that the key element will appear anywhere in the sorted list.
+* Analyze Binary Search with the worst-case scenario: when the
+  important element is at the end of the sorted list or not present at all.
+* Compare Binary Search vs. Sequential Search: When looking for an
+  element in a list, this compares the speed and effectiveness of
+  Binary Search and Sequential Search.
+
+3. __Determine order of growth for a given recurrence__: Determines the order
+of growth for a given recurrence which are implemented in the file
+__recurrences.py__ by accepting three integer inputs: a, b and d.
+
+*a*: A integer number positive greather than 1. <br>
+*b*: A integer number positive greather than 1. <br>
+*d*: A integer number positive greather than 1. <br>
+  
+Since the inputs a, b, and d have been validate, we will apply the 
+  __master theorem__ which states:
+  
+* If **a < b^d**: The time complexity is *Θ(n^d)*. 
+* If **a == b^d**: The time complexity is *Θ(n^d log n)*. 
+* If **a > b^d**: The time complexity is *Θ(n^(log_b a))*.
+
+Based on the recurrence, this figures out the asymptotic behavior 
+of recursive algorithms.
+
+4. __Apply binary exponentation__: This program analyzes effective algorithm
+that uses binary representation to divide the exponentiation process into
+a number of smaller, more manageable steps in order to compute large powers
+of a number. The file **binary_exponentiation.py** contains implementations
+for binary exponentiation algorithms.
+   
+* The __base__ is set to 2 by default and must be a positive integer.
+* The __exponent__ must be a positive integer.
+
+The program applies two (2) methods of binary exponentiation:
+  * __Left-to-right binary exponentiation__: With this method, the exponentiation
+  is calculated by looking at the exponent's bits from left to right.
+  * __Right-to-left binary exponentiation__: With this method, the exponentiation
+  is calculated by looking at the exponent's bits from right to left.
+
+5. __Apply Horspool's algorithm__
+* __To search words__: In the string search algorithm that effectively looks
+  for a pattern in a text by calculating how much to shift the pattern upon
+  a mismatch using a shift table.
+   
+The required input to this function:
+  * After the user enters a word or phrase (text), it is converted to uppercase
+    for uniform comparison. For instance, you can try: "JIM_SAW_A_BARBERSHOP".
+  * After the user enters a word (pattern), it is converted to uppercase
+    for uniform comparison. For instance, you can try: "BARBER".
+
+* **To search integers**: To search for integers we can interpret the binary
+  pattern matching in the context of a binary string, where the pattern is a
+  sequence of 1s and 0s in binary.
+
+  The required input to this function:
+  * After the user inputs a binary integer, the text is repeated by converting
+    it to a string and multiplying it. For instance, you can try: "1000".
+  * After the user inputs a binar pattern, the patter is search for in the
+    binary string. For instance, you can try: "00001".
+
+6. **Exit**: This option allows you to exit the program.
+
+**Solution:** This solution includes multiple Python files that implement various 
+algorithms for solving problems related to sorting, searching, and exponentiation.
+* [main](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/main.py)
+* [sorting](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/sorting.py)
+* [binary_exponetiation](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/binary_exponetiation.py)
+* [searching](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/searching.py)
+* [horspool](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/horspool.py)
+* [recurrences](https://raw.githubusercontent.com/cristian9217/cristian9217/default/courses/CECS6010/recurrences.py)
---
+++ courses/CECS6010/searching.py
@@ -0,0 +1,285 @@
+"""
+Module Name: searching.py
+Description: A Python module implementing searching 
+algorithms such as Binary Search for average case, 
+worst case adn comparing linear and binary search. 
+Author: Cristian M. Pagan
+Date: February 14, 2024
+"""
+
+import math
+import time
+
+class ArraySearching:
+    """
+    A class to implement various search algorithms like
+    Binary Search, with average and worst-case analysis.
+    
+    Attributes:
+        arr (list): The list of numbers.
+        length (int): The length of the array.
+    """
+    def __init__(self, arr):
+        """ Initializes the ArraySearching class with the given array."""        
+        self.setArray(arr)
+        self.setLength()
+
+    def setArray(self, arr):
+        """ Sets the array filtering out negatives values. """
+        if len(arr) == 0:
+            arr = [3, 14, 27, 31, 39, 42, 55, 70, 74, 81, 85, 93, 98]
+        else:
+            arr = sorted([x for x in arr if x >= 0])
+        self.arr = arr
+
+    def setLength(self):
+        """ Sets the length of the array."""
+        self.length = len(self.arr)
+        
+    # Performing Binary Search with average-case
+    def average_binary_search(self):
+        """ 
+        Performs Binary Search on the array to calculate the 
+        average case for a successful search.
+        a) Calculates log_2(n + 1).
+        b) Identifies the numbers that require the most comparisons.
+        c) Computes the average number of comparisons.
+        """
+        print(f"a) log_2(n+1) = log_2({self.length} + 1) = "
+            f"{self.largest_key()}\n")
+        
+        total_count = 0
+        count_dict = {}
+        
+        print("Index\tNumber\tCount")
+        for num in self.arr:
+            count = self.binary_search_average(num)
+            count_dict[num] = count
+            mid = self.arr.index(num) + 1
+            
+            print(f"{mid:^5}\t{num:^5}\t{count:^5}")
+            total_count += count 
+            
+        print(f"b) {self.largest_number_key(count_dict)}")
+        
+        avg = self.average(total_count, self.length)
+        print(f"c) Average: {total_count} / {self.length} = {avg:.7f}")
+        
+    def largest_key(self):
+        """ Calculates the largest key based on the array length. """
+        return math.ceil(math.log2(self.length))
+        
+    def binary_search_average(self, num):
+        """ Performs Binary Search for the average case on a 
+        given number. """    
+        low = 0
+        high = len(self.arr) - 1
+        count = 0
+        
+        while low <= high:
+            mid = (low + high) // 2
+            count += 1
+
+            if num < self.arr[mid]:
+                high = mid - 1
+            elif num > self.arr[mid]:
+                low = mid + 1
+            else:
+                break
+        return count
+            
+    def largest_number_key(self, count_dict):
+        """ Identifies the numbers that required the largest 
+        number of comparisons during Binary Search. """
+        list = []
+        for key, value in count_dict.items():
+            if value == self.largest_key():
+                list.append(key)
+        return list    
+
+    def average(self, a, b):
+        """ Calculates the average by dividing the total count 
+        by the number of elements. """
+        return a / b if b != 0 else a
+    # End of the performing Binary Search with average-case
+
+    # Performing Binary Search with worst-case
+    def worst_binary_search(self):
+        """ 
+        Simulates Binary Search in the worst case by introducing 
+        a new element into the array, which increases the length. 
+        This helps analyze the worst-case performance.
+        """
+        def new_list(index : int, number : int) -> list:
+            """ Generate a new list for new length """
+            aux_nums = [num + 1 for num in self.arr]
+            aux_nums.insert(index, number)
+            return sorted(aux_nums)
+        
+        aux_nums = new_list(3, 2)
+        
+        print(f"\nThe new list of numbers: {aux_nums}")
+        print(f"The new length of the array: {len(aux_nums)}")
+        
+        total_count = 0
+        count_dict = {}
+        
+        print("Index\tNumber\tCount")
+        for num in aux_nums:
+            count = self.binary_search_worst(num)
+            count_dict[num] = count
+            mid = aux_nums.index(num) + 1
+            
+            print(f"{mid:^5}\t{num:^5}\t{count:^5}")
+            total_count += count
+        
+        avg = self.average(total_count, len(aux_nums))
+        print(f"c) Average: {total_count} / {len(aux_nums)} = {avg:.7f}")
+    
+    def binary_search_worst(self, num):    
+        """ Performs Binary Search for the worst-case scenario on 
+        a given number. """
+        low = 0
+        high = len(self.arr) - 1
+        count = 0
+        
+        while low <= high:
+            mid = (low + high) // 2
+            count += 1
+
+            if num < self.arr[mid]:
+                high = mid - 1
+            elif num > self.arr[mid]:
+                low = mid + 1
+            else:
+                low = high + 1
+        return count
+    # End of the Performing Binary Search with worst-case
+    
+    def __str__(self):
+        """ Returns a string representation of the array. """
+        return str(self.arr)
+
+class BinaryVsSequentialSearch:
+    """  A class to compare the performance of Binary Search 
+    (O(log n)) and Sequential Search (O(n)).
+    
+    Attributes:
+        base (int): The base number used in the search.
+        exponent (int): The exponent used to compute the size.
+    """
+    def __init__(self, base, exponent):
+        self.base = self.setBase(base)
+        self.exponent = self.setExponent(exponent)
+    
+    def setBase(self, base):
+        """ Validates and sets the base number for search calculations. 
+        If the base is less than or equal to 0, it defaults to 10."""
+        base = int(base)
+        if base > 0:
+            return base
+        else: 
+            return 10
+    
+    def setExponent(self, exponent):
+        """ Validates and sets the exponent for search calculations. 
+        If the exponent is less than or equal to 0, it defaults to 5."""
+        exponent = int(exponent)
+        if exponent > 0:
+            return exponent
+        else:
+            return 5
+        
+    def getNumber(self):
+        """ Computes the size of the search space using the 
+        formula `base^(exponent - 1)`. """
+        return self.base ** (self.exponent - 1)
+                    
+    def get_sequential_average(self):
+        """ Returns the average number of comparisons 
+        for Sequential Search (O(n). """
+        return self.getNumber()
+
+    def get_binary_average(self):
+        """ Returns the average number of comparisons for 
+        Binary Search (O(log n)) by calculating the logarithm 
+        of the base to the base 2."""
+        return math.log(self.base, 2)
+
+    def average(self):
+        """ Computes and returns the ratio of the average number 
+        of comparisons between Sequential and Binary Search. """
+        linear = self.get_sequential_average()
+        binary = self.get_binary_average()
+        results = (linear / binary)
+        return round(results / 1000) * 1000
+
+def main():
+    while True: 
+        print("\nMenu of Searching Algorithms:")
+        print("1. Return to the Main Menu")
+        print("2. Analyze Binary Search with average-case scenario")
+        print("3. Analyze Binary Search with worst-case scenario")
+        print("4. Analyze Binary Search vs. Sequential Search")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            return
+        elif choice == '2':
+            arr_num = []
+            
+            print("\nAverage-case scenario in successful search...")
+            user_input = input("Enter numbers separated by commas: ")
+            try:
+                numbers = [int(num.strip()) for num in user_input.split(',')]
+
+                if any(num < 0 for num in numbers):
+                    print(f"Invalid input: {numbers}")
+                else:
+                    arr_num.extend(numbers)
+            except ValueError:
+                print("Invalid input! Please enter only numbers.")
+            
+            search = ArraySearching(arr_num)
+            print(f"\nThe array is: {search}")
+            print(f"The length is: {search.length}\n")
+            search.average_binary_search()
+
+        elif choice == '3':
+            arr_num = []
+            
+            print("\nWorst-case scenario in unsuccessful search...")
+            user_input = input("Enter numbers separated by commas: ")
+            try:
+                numbers = [int(num.strip()) for num in user_input.split(',')]
+
+                if any(num < 0 for num in numbers):
+                    print(f"Invalid input: {numbers}")
+                else:
+                    arr_num.extend(numbers)
+            except ValueError:
+                print("Invalid input! Please enter only numbers.")
+
+            search = ArraySearching(arr_num)
+            print(f"\nThe array is: {search}")
+            print(f"The length of the array is: {search.length}")
+            search.worst_binary_search()
+
+        elif choice == '4':
+            try:
+                print("Sequential vs Binary Search...")                
+                base = input("Enter the base: ")
+                exponent = input("Enter the exponent: ")
+                
+                bs = BinaryVsSequentialSearch(base, exponent)
+                
+                print("\nEstimating how many times faster search...")
+                time.sleep(2)
+                print(f"Sequential: {bs.get_sequential_average()}")
+                print(f"Binary:     {bs.get_binary_average():.6f}")
+                print(f"Result:     {bs.average()}")
+            except ValueError:
+                print("Invalid input!")
+        else:
+            print("Invalid choice! Please enter a valid option.")
